4653ff6ade9f0aaf4ca707b639f7c14dc198464c
MickeyOuse <47369602+RoninSunshine@users.noreply.github.com>
2024-11-20 14:40:18 +0100

    Restructuration de l'arborescence du projet actuel pour qu'il ait le même que celui que j'avais fait et qui est la précédente version (ProjectV1)

--- web_app_frontend/src/router/index.js
+++ web_app_frontend/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import ConnectionPage from '../components/ConnectionPage.vue';
+import ConnectionPage from '../View/ConnectionPage.vue';
 import TheWelcome from '../components/TheWelcome.vue';
 
 const routes = [
@@ -16,7 +16,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes,
 });
 
