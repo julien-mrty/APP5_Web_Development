bdb1531fb0f17414dbb70ddb3d8c5cd952c856b3
TZ-Boomer <julienmarty0819@gmail.com>
2024-11-20 15:53:04 +0100

    Added home, scores and play page and link between this pages. Still need to link connection and home page properly.

--- web_app_frontend/src/main.js
+++ web_app_frontend/src/main.js
@@ -5,5 +5,5 @@ import App from './App.vue';
 import router from './router';
 
 createApp(App)
-  .use(router)
-  .mount('#app');
+  .use(router) // Use the router in the app
+  .mount('#app'); // Mount the app to the #app element
