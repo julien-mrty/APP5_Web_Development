91f21b5001dc3996d13b12da31e92c48767ef92d
TZ-Boomer <julienmarty0819@gmail.com>
2024-12-14 19:31:22 +0100

    Change syntax to pass linters

--- web_app_frontend/eslint.config.js
+++ web_app_frontend/eslint.config.js
@@ -6,7 +6,7 @@ import pluginVue from "eslint-plugin-vue";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,vue}"] },
-  { languageOptions: { globals: globals.browser, ...globals.node } },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...pluginVue.configs["flat/essential"],
 ];
