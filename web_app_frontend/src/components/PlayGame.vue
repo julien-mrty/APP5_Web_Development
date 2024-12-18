8f6f627b352814f6aa3fe1ea5d059ea5ad562d37
TZ-Boomer <julienmarty0819@gmail.com>
2024-12-18 15:11:59 +0100

    Fixed an error in the number of possible cards.

--- web_app_frontend/src/components/PlayGame.vue
+++ web_app_frontend/src/components/PlayGame.vue
@@ -6,7 +6,7 @@
       <button @click="logout" class="logout-button">Logout</button>
     </div>
 
-    <hr/>
+    <hr />
     <h2>Play the Card Matching Game</h2>
 
     <div class="game-settings">
@@ -29,19 +29,10 @@
 
     <!-- Plateau de jeu -->
     <section class="game-board" v-if="!gameEnded && !newPlayer">
-      <div 
-        v-for="(card, index) in cards" 
-        :key="index"
-        class="card"
-        :class="{ 'is-flipped': card.visible }"
-        @click="flipCard(index)"
-      >
+      <div v-for="(card, index) in cards" :key="index" class="card" :class="{ 'is-flipped': card.visible }"
+        @click="flipCard(index)">
         <div class="card-face is-front">
-          <img 
-            class="card-image" 
-            :src="`/images/${card.value}.png`" 
-            :alt="card.value"
-          />
+          <img class="card-image" :src="`/images/${card.value}.png`" :alt="card.value" />
           <img v-if="card.matched" src="/images/checkmark.svg" class="icon-checkmark" />
         </div>
         <div class="card-face is-back"></div>
@@ -104,7 +95,7 @@ export default {
     ]; // 11 images
 
     // Options possibles pour le nombre de cartes (maximum 22)
-    const cardCountOptions = [4, 8, 12, 16, 20, 22];
+    const cardCountOptions = [4, 8, 12, 16, 20, 24];
     const selectedCardCount = ref(4); // Valeur par défaut, 4 cartes
 
     const newPlayer = ref(true);
