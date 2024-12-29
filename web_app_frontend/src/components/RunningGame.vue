3965ef984a74b77504cf19ca542e0e91b17591c5
MickeyOuse <47369602+RoninSunshine@users.noreply.github.com>
2024-12-29 21:41:13 +0100

    Added a button and method for accessing the running game from the home page. Changed the text of the buttons in the home page

--- web_app_frontend/src/components/RunningGame.vue
+++ web_app_frontend/src/components/RunningGame.vue
@@ -1,22 +1,24 @@
 <template>
-
     <div class="game_page">
         <h1>You're in the game page</h1>
         <div id="phaser">  </div>
     </div>
-
-    
-    
 </template>
 
 
 <script>
-import startGame from "../game/game.js";
+import { startGame, destroyGame } from "../game/game.js";
 
 export default {
     mounted() {
-        startGame()
-        //console.log(game); // Vérifiez si le jeu est bien importé
+        //Starting the game
+        this.gameInstance = startGame()
+        console.log("Game Started", this.gameInstance); // Check that the game has been imported
+    },
+    beforeUnmount() {
+        // Destroys the Phaser instance
+        destroyGame();
+        console.log("Game instance destroyed");
     },
 };
 
